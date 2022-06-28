b86bd074f588d89c6491281ff250419771300e05
Alejandro Randon <58668589+alerandon@users.noreply.github.com>
2022-06-27 22:32:48 -0400

    added dices and numbers UI

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,8 +1,17 @@
+<script setup>
+defineProps({
+    show: {
+        type: Boolean,
+        required: true
+    }
+})
+</script>
+
 <template>
     <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="relative inline-block text-left">
         <div>
-            <button type="button"
+            <button @click="show = !show" type="button"
                 class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
                 id="menu-button" aria-expanded="true" aria-haspopup="true">
                 Classic
@@ -26,14 +35,13 @@
       From: "transform opacity-100 scale-100"
       To: "transform opacity-0 scale-95"
   -->
-        <div class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+        <div v-if="show"
+            class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
             role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
             <div class="py-1" role="none">
                 <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
                 <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
                     id="menu-item-0">Classic</a>
-                <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-                    id="menu-item-1">3D</a>
             </div>
         </div>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,32 @@ import TheWelcome from "@/components/TheWelcome.vue";
 </script>
 
 <template>
-  <main class="">
-    <p class="text-center text-3xl">gaga</p>
+  <main class="pt-24 px-5">
+    <div class="w-3/6 mx-auto">
+      <div class="w-full flex space-x-7 items-center justify-center">
+        <img src="src\assets\icons\dices\d4.png" alt=" " />
+        <img src="src\assets\icons\dices\d6.png" alt=" " />
+        <img src="src\assets\icons\dices\d8.png" alt=" " />
+        <img src="src\assets\icons\dices\d10.png" alt=" " />
+        <img src="src\assets\icons\dices\d12.png" alt=" " />
+        <img src="src\assets\icons\dices\d20.png" alt=" " />
+      </div>
+      <div
+        class="bg-yellow-300 w-full h-20 max-w-2xl mx-auto border rounded-xl mt-6 py-1 flex items-center justify-center">
+        <p class="text-4xl">0</p>
+      </div>
+      <div class="flex justify-center pt-5">
+        <button class="w-24 text-white text-center text-xl py-1 bg-gray-500 border rounded-md">
+          Clear
+        </button>
+        <button class="w-24 text-white text-center text-xl py-1 ml-2 bg-yellow-600 border rounded-md">
+          Roll
+        </button>
+      </div>
+      <div class="text-center pt-20">
+        <h3 class="text-7xl">0</h3>
+        <p class="pt-4">0d0 + 0d0 = (0 + 0) + (0 + 0)</p>
+      </div>
+    </div>
   </main>
 </template>
